c5e50d7ae0974d350813b5ea3cc8c7c3e6d493c4
Jacob <jake.arsenault@hotmail.ca>
2020-10-16 16:34:56 -0400

    Added Design/Mockup, added android studio project

---
+++ JDSecuritySolutions/loginandregister/src/main/java/jacob/daniel/loginandregister/LoginAndRegister.java
@@ -0,0 +1,14 @@
+package jacob.daniel.loginandregister;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LoginAndRegister extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_and_register);
+    }
+}
