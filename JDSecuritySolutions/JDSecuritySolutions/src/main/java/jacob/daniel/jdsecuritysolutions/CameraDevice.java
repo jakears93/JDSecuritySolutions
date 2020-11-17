00e039fefcd82d6b70a674df6c89d9b68138a5f3
Jacob Arsenault <jake.arsenault@hotmail.ca>
2020-11-17 17:27:07 -0500

    changed cameraDevice to use mediarecorder to allow for background filming

--- JDSecuritySolutions/JDSecuritySolutions/src/main/java/jacob/daniel/jdsecuritysolutions/CameraDevice.java
+++ JDSecuritySolutions/JDSecuritySolutions/src/main/java/jacob/daniel/jdsecuritysolutions/CameraDevice.java
@@ -1,79 +1,134 @@
 package jacob.daniel.jdsecuritysolutions;
 
+import android.Manifest;
 import android.app.AlertDialog;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.content.pm.ResolveInfo;
 import android.graphics.SurfaceTexture;
+import android.hardware.Camera;
+import android.media.MediaRecorder;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.os.Environment;
 import android.provider.MediaStore;
+import android.view.Surface;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
 import android.view.TextureView;
 import android.view.View;
 import android.widget.Toast;
 import android.widget.VideoView;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
 import java.io.File;
+import java.io.IOException;
 import java.util.List;
+
+//TODO change video orientation
+//TODO add surfaceview to layout
+//TODO reorganize code structure
 
 public class CameraDevice extends AppCompatActivity {
 
-    private static final int MAX_PREVIEW_WIDTH = 1920;
-    private static final int MAX_PREVIEW_HEIGHT = 1080;
-
+    private static final int MAX_PREVIEW_WIDTH = 1280;
+    private static final int MAX_PREVIEW_HEIGHT = 720;
+    private boolean allowRecord = false;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.camera_device);
-        dispatchTakeVideoIntent();
-    }
-
-    static final int REQUEST_VIDEO_CAPTURE = 1;
-
-
-    private void dispatchTakeVideoIntent() {
-        if(hasCamera()){
-            Intent takeVideoIntent = new Intent(MediaStore.ACTION_VIDEO_CAPTURE);
-            takeVideoIntent.putExtra(MediaStore.EXTRA_DURATION_LIMIT, 10);
-            startActivityForResult(takeVideoIntent, REQUEST_VIDEO_CAPTURE);
+        checkPermissions();
+        if(allowRecord) {
+            try {
+                dispatchTakeVideoIntent();
+            } catch (IOException e) {
+                e.printStackTrace();
+                Toast toast = Toast.makeText(getApplicationContext(), "IOEXCEPTION", Toast.LENGTH_SHORT);
+                toast.show();
+            }
         }
         else{
-            Toast toast = Toast.makeText(getApplicationContext(), "FAILED TAKING VIDEO", Toast.LENGTH_SHORT);
+            Toast toast = Toast.makeText(getApplicationContext(), "Cannot Record", Toast.LENGTH_SHORT);
             toast.show();
         }
     }
 
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        if(resultCode == RESULT_OK && requestCode == REQUEST_VIDEO_CAPTURE){
-            AlertDialog.Builder builder = new AlertDialog.Builder(this);
-            VideoView videoView = new VideoView(this);
-            videoView.setVideoURI(data.getData());
-            videoView.start();
-            builder.setView(videoView).show();
+    private void dispatchTakeVideoIntent() throws IOException {
+            final String fileName =  Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DCIM) + File.separator + "test.mp4";
+            final MediaRecorder recorder = new MediaRecorder();
+
+            SurfaceTexture sft = new SurfaceTexture(0);
+            Surface sf = new Surface(sft);
+
+            recorder.setPreviewDisplay(sf);
+            recorder.setAudioSource(MediaRecorder.AudioSource.MIC);
+            recorder.setVideoSource(MediaRecorder.VideoSource.CAMERA);
+            recorder.setOutputFormat(MediaRecorder.OutputFormat.MPEG_4);
+            recorder.setOutputFile(fileName);
+            recorder.setVideoFrameRate(60);
+            recorder.setVideoSize(MAX_PREVIEW_WIDTH, MAX_PREVIEW_HEIGHT);
+            recorder.setVideoEncoder(MediaRecorder.VideoEncoder.DEFAULT);
+            recorder.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB);
+
+
+            recorder.prepare();
+            try {
+                recorder.start();
+                new CountDownTimer(10000, 1000) {
+
+                    public void onTick(long millisUntilFinished) {
+
+                    }
+
+                    public void onFinish() {
+                        Toast toast=Toast.makeText(getApplicationContext(),"DONE",Toast.LENGTH_SHORT);
+                        toast.show();
+                        recorder.stop();
+                        recorder.reset();   // You can reuse the object by going back to setAudioSource() step
+                        recorder.release(); // Now the object cannot be reused
+                        VideoView screen = findViewById(R.id.video);
+                        screen.setVideoURI(Uri.parse(fileName));
+                    }
+                }.start();
+            }catch(Exception ex){
+                ex.printStackTrace();
+                Toast toast=Toast.makeText(getApplicationContext(),"Failed To Start Recorder",Toast.LENGTH_SHORT);
+                toast.show();
+            }
+    }
+
+    public void flippedSwitch(View v) {
+
+    }
+
+    public void checkPermissions() {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.RECORD_AUDIO) != PackageManager.PERMISSION_GRANTED) {
+            String[] permissions = {Manifest.permission.RECORD_AUDIO, Manifest.permission.CAMERA};
+            ActivityCompat.requestPermissions(this, new String[] { Manifest.permission.RECORD_AUDIO }, 10);
         }
         else{
-            Toast toast=Toast.makeText(getApplicationContext(),"FAILED VIEWING VIDEO",Toast.LENGTH_SHORT);
-            toast.show();
+            allowRecord = true;
+        }
+
+    }
+
+    @Override public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == 10) {
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED && grantResults[1] == PackageManager.PERMISSION_GRANTED) {
+                allowRecord = true;
+            }else{
+                Toast toast = Toast.makeText(getApplicationContext(), "FAILED GAINING PERMISSIONS", Toast.LENGTH_SHORT);
+                toast.show();
+            }
         }
     }
 
-    public void flippedSwitch(View v){
-
-    }
-
-    private boolean hasCamera() {
-        if (getPackageManager().hasSystemFeature(
-                PackageManager.FEATURE_CAMERA_FRONT)){
-            return true;
-        } else {
-            return false;
-        }
-    }
 }
