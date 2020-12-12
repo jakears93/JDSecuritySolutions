b0b54f1d154b54dbc4f4c185b00219dfb4e1628b
Jacob <jake.arsenault@hotmail.ca>
2020-12-11 19:58:16 -0500

    removed extra delay from splashscreen that was preventing login page from loading for 5 seconds

--- JDSecuritySolutions/JDSecuritySolutions/src/main/java/jacob/daniel/jdsecuritysolutions/LoginAndRegister.java
+++ JDSecuritySolutions/JDSecuritySolutions/src/main/java/jacob/daniel/jdsecuritysolutions/LoginAndRegister.java
@@ -50,14 +50,6 @@ public class LoginAndRegister extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //Display Splashscreen
-        setContentView(R.layout.splashscreen);
-        try {
-            Thread.sleep(3000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-
         Configuration orientation = getResources().getConfiguration();
         onConfigurationChanged(orientation);
         attemptAutoLogin();
@@ -75,11 +67,6 @@ public class LoginAndRegister extends AppCompatActivity {
                 startActivityForResult(signInIntent, RC_SIGN_IN);
             }
         });
-    }
-
-    @Override
-    protected void onStart() {
-        super.onStart();
     }
 
     @Override
